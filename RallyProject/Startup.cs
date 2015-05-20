7335d7c2ecb389a68f61e1aea4a69d1e56f73a33
Lauren <laurenholloway1010@gmail.com>
2015-05-20 14:28:01 -0400

    initial push for setup

---
+++ RallyProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RallyProject.Startup))]
+namespace RallyProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
